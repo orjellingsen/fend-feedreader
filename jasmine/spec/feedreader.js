88883d86bbdc50ce537aedc257744e029035c715
orjellingsen <orjanell@gmail.com>
2016-11-06 00:36:14 +0100

    Added all test 'it' functions

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -56,22 +56,31 @@ $(function() {
        * the CSS to determine how we're performing the
        * hiding/showing of the menu element.
        */
+       it('is hidden by default', function() {
+
+       });
 
        /* TODO: Write a test that ensures the menu changes
         * visibility when the menu icon is clicked. This test
         * should have two expectations: does the menu display when
         * clicked and does it hide when clicked again.
         */
+        it('change visibility when menu icon is clicked', function() {
+
+        });
     });
 
     /* Write a new test suite named "Initial Entries" */
-    describe('Initial Entries',function() {
+    describe('Initial Entries', function() {
       /* TODO: Write a test that ensures when the loadFeed
        * function is called and completes its work, there is at least
        * a single .entry element within the .feed container.
        * Remember, loadFeed() is asynchronous so this test will require
        * the use of Jasmine's beforeEach and asynchronous done() function.
        */
+       it('has at least one entry', function() {
+
+       });
     });
 
 
@@ -81,5 +90,8 @@ $(function() {
        * by the loadFeed function that the content actually changes.
        * Remember, loadFeed() is asynchronous.
        */
+       it('changes content when new feed is loaded', function() {
+
+       });
     });
 }());
