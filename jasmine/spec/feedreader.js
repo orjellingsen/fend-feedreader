0dd74f10dbcbc06f6647c906bea382589a600b38
orjellingsen <orjanell@gmail.com>
2016-11-06 01:29:25 +0100

    Test: 'has at least one entry' Passed. Tested by removing entry html so no entries can show

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -82,14 +82,21 @@ $(function() {
 
     /* Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
+      beforeEach(function(done) {
+        loadFeed(0, function() {
+          done();
+        });
+      });
       /* TODO: Write a test that ensures when the loadFeed
        * function is called and completes its work, there is at least
        * a single .entry element within the .feed container.
        * Remember, loadFeed() is asynchronous so this test will require
        * the use of Jasmine's beforeEach and asynchronous done() function.
        */
-       it('has at least one entry', function() {
-
+       it('has at least one entry', function(done) {
+         var entryLength = $('.entry').length;
+         expect(entryLength).not.toBe(0);
+         done();
        });
     });
 
